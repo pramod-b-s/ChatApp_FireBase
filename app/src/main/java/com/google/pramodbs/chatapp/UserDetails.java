1a60d8d6db5b8570382f4fa7b9f8dd9e816e9a1f
Pramod B.S <prmdbs3@gmail.com>
2017-03-09 21:46:37 +0530

    commit1

---
+++ app/src/main/java/com/google/pramodbs/chatapp/UserDetails.java
@@ -0,0 +1,11 @@
+package com.google.pramodbs.chatapp;
+
+/**
+ * Created by Pramod B.S on 08-03-2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
